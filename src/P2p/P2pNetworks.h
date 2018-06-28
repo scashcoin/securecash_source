767023a0d43dc90f6e51fb8da3ddf1e9d5844e05
eGame Cash <37266904+egamecash@users.noreply.github.com>
2018-06-28 16:45:52 -0300

    Initial commit

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,25 @@
+// Copyright (c) 2012-2017, The CryptoNote developers, The Bytecoin developers
+//
+// This file is part of Bytecoin.
+//
+// Bytecoin is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Bytecoin is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Bytecoin.  If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid BYTECOIN_NETWORK = { {  0xb5, 0xe3, 0x4a, 0x6c, 0xcf, 0xe3, 0x57, 0x41, 0xe3, 0xe3, 0x91, 0xe3, 0xb6, 0xc1, 0x43, 0xe3  } };
+
+}
+
